d742926f3187ea493320114aef3917da3c7924af
1Day@atime <34130768+Demidomo@users.noreply.github.com>
2022-05-16 21:23:09 -0700

    Create ProblemSet2.py

---
+++ ProblemSet2.py
@@ -0,0 +1,23 @@
+// Have the function BracketCombinations(num) read num which will be an integer greater than or equal to zero, and return the number of valid combinations that can be formed with num pairs of parentheses
+// Examples Input: 3 Output: 5
+
+function BracketCombinations(num) {
+
+// code goes here
+count = 0; n = 2**(2num); 
+
+for (iperm=0; iperm<n; iperm++){ 
+
+    sum = 0; i = iperm; 
+
+for (dig = 2num; dig > 0; dig --){ 
+
+  bit = i % 2; 
+  i -= bit; 
+  i /= 2; 
+  sum = sum + (bit == 0?1:-1); 
+
+if(sum<0){break; }; }; 
+  count += (sum ==0)?1:0; }; return count; 
+
+}
