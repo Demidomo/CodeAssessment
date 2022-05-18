1fda1788dbd0cedab423a1a55e2413b36ae64b66
1Day@atime <34130768+Demidomo@users.noreply.github.com>
2022-05-17 19:00:22 -0700

    Create BigO.js

---
+++ BigO.js
@@ -0,0 +1,24 @@
+// Big O 
+
+// why: 
+// We use it to judge which solution is 'better' 
+//Solutions that use the least time + space
+
+
+// Big O can be objective with determining which solution is more or less optimizing 
+// Builds resillent programs. 
+
+
+
+const calculateAverage = (numbers) => { 
+  let sum = 0;
+  
+   for (let i = 0; i < numbers.length; i++) { 
+     let numbers = numbers[i];
+     sum += number; 
+   }
+   return sum / numbers.length; // calculateAverage is sum divided by numbers count 
+
+};
+
+console.log(calculateAverage([2, 3, 4, 1])); //prints calculateAverage, in this case it is 2.5
