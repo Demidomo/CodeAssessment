a368ecd5afe471ed570e61e4a949b596d88080ae
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-05-19 16:13:43 -0700

    Update and rename Common_Algorithms to CommonAlgorithm.py

---
+++ CommonAlgorithm.py
@@ -0,0 +1,29 @@
+# sliding window
+# maximum sum of a subarray
+
+# Two pointers 
+# removing duplicates
+
+# Fast and slow pointers 
+# find a cycle in a linked list 
+
+# Merge Intervals 
+# Conflicting appointments 
+
+# Breadth first search
+# binary tree level order traversal 
+
+# Depth first search
+# find path with given sequence
+
+# Subsets 
+# permutations
+
+# Top 'K' elements
+# Kth smallest number
+
+# Dynamic programming
+# knapsack problem 
+
+
+
