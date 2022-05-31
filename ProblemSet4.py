771f476b6474fc837556182a5d8be084d42add02
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-05-30 21:53:05 -0700

    Update ProblemSet4.py

--- ProblemSet4.py
+++ ProblemSet4.py
@@ -17,3 +17,27 @@ class Node {
     n.next = end;
   }
 }
+
+
+
+
+#Deleting a node from a linked list
+
+
+Node deleteNode (Node head, int d) {
+  Node n = head;
+  
+  if (n.data == d) {
+    return head.next; #moved head
+  }
+
+  while (n.next != null) {
+    if (n.next.data == d) {
+      n.next = n.next.next; 
+      return head; 
+      
+    }
+    n = n.next;
+  }
+  return head;
+}
