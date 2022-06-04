9382d7b84c1d8c05b110fbc866617ec42f4bc8b6
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-04 16:00:09 -0700

    Update BinarySearch.js

--- BinarySearch.js
+++ BinarySearch.js
@@ -14,21 +14,30 @@
 // Binary search that's gonna take in an array and target
 let nums = [2, 7, 8, 9, 10, 13, 17, 19, 21];
 
-function binarySearch(array, target) {
-  let left = 0;                         // Setting left marker 
-  let right = array.length - 1;         // Setting right marker 
+functoin binarySearch(array, target) { 
+         return binarySearchHelper(array, target, 0, array.length - 1)
+                  }
 
-  while(left <= right) {                          // determining how to loop, keep searching until there isn't elements left to search 
+function binarySearchHelper(array, target, left, right) {
+         if(left > right) { 
+              return false;
+         } 
+// let left = 0;                         // Setting left marker 
+// let right = array.length - 1;         // Setting right marker 
+
+// while(left <= right) {                          // determining how to loop, keep searching until there isn't elements left to search 
     let mid = Math.floor((left + right) / 2)      // with each search we determine what the mid is. Add math.floor in case of decimal
     if(target === array[mid]) {                   // mid = index 4 
       return mid;                                 // return the mid value if mid value is the target 
     } else if (target < array[mid]) {             // if traget is on the left hand side of mid 
-      right = mid - 1;                            // new right will be the index next to the mid 
+     return binarySearchHelper(array, target, left, mid - 1); 
+     // right = mid - 1;                            // new right will be the index next to the mid 
     } else {                                      // otherwise if target is on the right hand side
-      left = mid + 1;                             // new left will be the index on the right of mid 
+     // left = mid + 1;                             // new left will be the index on the right of mid 
+     return binarySearchHelper(array, target, mid + 1, right); 
     } 
-  } 
-   return false;                                  // false value will be returned if target cannot be found in array 
+//  } 
+//   return false;                                  // false value will be returned if target cannot be found in array 
    
 
 }
