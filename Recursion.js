32e20b31aa0d8a48197043b1cf509cc68025949b
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-04 15:24:24 -0700

    Update Recursion.js

--- Recursion.js
+++ Recursion.js
@@ -1,1 +1,38 @@
+// Recursion is used for problems that contain a smaler instances of the ame problem
+
+
+
+// - Base case: smallest instance, automatically know the answer
+// - Recursive case: the problem shrinks the input towards the base case 
+
+
+
+
+// Write a function 'factorial' that takes a number
+// as an argument and returns the factorial of that 
+// number. 
+//
+// The factorial of 'n' is the product of all positive
+// numbers from 1 to 'n'. 
+
+
+factorial(1); //1
+factorial(2); //2
+factorial(3); //6
+factorial(4); //24 
+factorial(5); //120 
+
+// factorial(n) = n * factorial(n - 1) 
+// fact(4) = 4 * fact(3) 
+// fact(3) = 3 * fact(2) 
+// fact(2) = 2 * fact(1) 
+// fact(1) base case
+
+
+
+const factorial = (n) => {
+  if (n === 1) return 1; 
+
+  return n * factorial(n - 1) ; 
+}; 
 
