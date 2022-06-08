f6f3ab244465671d120d3ecbd4d72626ac51b7f6
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-08 09:39:03 -0700

    Update string.js

--- string.js
+++ string.js
@@ -6,7 +6,9 @@
 // 
 // step 3: reverse the array with reverse function 
 // step 4: convert the array back into a string 
-
+//
+// .split converts a string into array 
+// .join converts array into string 
 
 
 var str = "hello world from earth"; 
@@ -19,3 +21,19 @@ console.log(rev); // [ 'earth', 'from', 'world', 'hello' ]
 
 var rev = arr.reverse().join(' ');
 console.log(rev); // earth from world hello
+
+
+// to reverse a string
+// step 1: make a string
+// step 2: convert the string into an array
+// step 3: reverse the array
+// step 4: convert back to string
+
+
+var str = "Matthew makes apps"; 
+var arr = str.split(' '); 
+
+var rev = arr.reverse(); 
+var str = rev.join(' '); 
+
+console.log(str); // apps makes Matthew
