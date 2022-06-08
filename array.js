4d82c0603e0565277dcbc7729d0ba4e4ac72557e
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-08 09:03:38 -0700

    Create array.js

---
+++ array.js
@@ -0,0 +1,19 @@
+var arr = [10, 'hello', true, (1,2,3)]; // to make an array
+
+// to loop through an array, use a for loop
+
+for (var i = 0; i < arr.length; i++) { // i is being set to 0, is looping incrementally 
+    arr[i] // everytime the inner statement is executed, i represents a different number
+    
+    // loop through each element
+}
+
+console.log(arr.length);
+
+
+// standalone sourcecode: 
+
+var arr = [10, true, '2343', (1, 3, 4)];
+
+for (var i = 0; i < arr.length; i++) {
+  
