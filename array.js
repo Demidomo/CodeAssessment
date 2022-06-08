3e029e1b8a2577595beb93889a7faba90083970d
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-08 09:19:36 -0700

    Update array.js

--- array.js
+++ array.js
@@ -22,3 +22,21 @@ var arr = [10, true, '2343', (1, 3, 4)];
 var arr = [10, 'hello', true, [1,2,3]]; // to make an array
 
 console.log(arr[3][1]); //2 
+
+console.log(arr.join('')); // 10hellotrue1,2,3
+console.log(arr.join(' ')); // 10 hello true 1,2,3
+
+
+console.log(arr); // [ 10, 'hello', true, [ 1, 2, 3 ] ]
+
+arr.push(1000);
+console.log(arr); // [ 10, 'hello', true, [ 1, 2, 3 ], 1000 ]
+
+arr.pop();
+console.log(arr); // [ 10, 'hello', true, [ 1, 2, 3 ] ]
+
+arr.shift(); 
+console.log(arr); // [ 'hello', true, [ 1, 2, 3 ] ]
+
+arr.reverse(); 
+console.log(arr); // [ [ 1, 2, 3 ], true, 'hello' ]
