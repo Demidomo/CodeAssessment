772683c86f4ddcd815424729a6411bff90b89b71
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-07 20:06:55 -0700

    Create PS.js

---
+++ PS.js
@@ -0,0 +1,13 @@
+
+
+// Remove Brackets
+function RemoveBrackets(str) { 
+  while (str.includes('()')) {
+      str = str.replace('()', '');
+  }
+  return str.length;
+
+}
+   
+// keep this function call here 
+console.log(RemoveBrackets(readline()));
