e06f9c7c334563843fccd64c3987c7ef8bac188a
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-08 10:13:38 -0700

    Update string.js

--- string.js
+++ string.js
@@ -37,3 +37,31 @@ var rev = arr.reverse();
 var str = rev.join(' '); 
 
 console.log(str); // apps makes Matthew
+
+
+
+
+// vowel count 
+// 
+// step 1: create a string
+// step 1.5: turn the string into an array
+// step 2: create a new array of vowels
+// step 3: make a for loop through the array
+// step 3.5: store a count of how many vowels 
+// step 4: within the for loop, check if each element contain the vowel characters
+// 
+
+var str = "hello coderbyte";
+var arr = str.split(''); 
+
+var vow = ['a','e','i','o','u'];
+var count = 0;
+
+for (var i = 0; i < arr.length; i++) {
+    if (vow.indexOf(arr[i])) {
+        count += 1;
+        
+    }
+}
+
+console.log(count);
