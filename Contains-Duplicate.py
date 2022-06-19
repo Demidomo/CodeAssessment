537f69f1e50c068d6e8d9b9a64030e25044bffc5
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-19 00:28:46 -0700

    Create Contains-Duplicate.py

---
+++ Contains-Duplicate.py
@@ -0,0 +1,16 @@
+
+// See if there is a duplicate in the array of numbers 
+// Brute force or Sort
+// 
+// Brute force:
+// Time O(n log n) 
+// Space O(1)
+// 
+// Sort using Hash set to detect duplicates 
+// Q2A: Does a certain value exist?
+// Once a number is in the hashset, the next time it shows up it is a duplicafte
+// 
+// Time and Space: O(n)
+  
+  
+  
