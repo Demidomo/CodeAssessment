5958369da48f4f3ffb9f5145e590cdd5757af9a8
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-19 16:24:57 -0700

    Create Group-Anagrams.swift

---
+++ Group-Anagrams.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var sortedStrToStrs = [String: [String]]()
+        
+        for str in strs {
+            let sortedStr = String(str.sorted())
+            
+            sortedStrToStrs[sortedStr, default: []].append(str)
+        }
+        
+        return Array(sortedStrToStrs.values)
+        
+    }
+}
