5fddfb7de0460a98a97b939b164b45517c70b407
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-19 12:15:33 -0700

    Create Valid-Anagram.js

---
+++ Valid-Anagram.js
@@ -0,0 +1,27 @@
+// Q2A: Does it have the exact same characters and quantity
+// if yes return true
+// if no return false 
+// 
+// same characters && same quantity 
+// 
+// 1. count the ocurrances of each character
+// Hashmap vs array
+// S hashmap & T hashmap
+// 
+// the value will be the count
+// the key will be the character 
+// 
+// S string: anagram
+// Hashmap S 
+// Key | Value
+// a | 3
+// n | 1
+// g | 1
+// r | 1
+// m | 1
+//
+//
+// we can then iterate through the hashmap's keys, make sure the count is the same for both hashmap/strings 
+
+//
+// Approach 1 may take up to O(s + t )
