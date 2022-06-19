9931a2d01f98f60ccd7e275b4cdf72805184a80f
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-19 12:26:22 -0700

    Create Valid-Anagram.py

---
+++ Valid-Anagram.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t): 
+            return False
+        
+        countS, countT = {}, {}    
+        
+        for i in range(len(s)): 
+            countS[s[i]] = 1 + countS.get(s[i], 0)  # if this key does not exist in the hash, the default value will return 0
+            countS[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:                            # c is the key
+            if countS[c] !== countT.get(c, 0):       # use get here if the k does not exist. Preventing a key error, and return 0
+                return False                        # if they are not the same, we know it's false
+            
+        return True                                 # if both negates and null, it will be true
