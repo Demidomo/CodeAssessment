f65ba0fceb8bce0f9340acbf3dcedb99af731852
progresso_espresso <34130768+SpaceGarten@users.noreply.github.com>
2022-06-20 22:16:18 -0700

    Update Product-of-array-except-self.py

--- Product-of-array-except-self.py
+++ Product-of-array-except-self.py
@@ -17,4 +17,25 @@
 # prefix   |  1  |  2  |  6  |  24 |
 # postfix     24    24    12    4
 #
-# result    
+# to save memory, instead of producing prefix and postfi x
+# we can have the product of the pairing prefix and postfix as the result output 
+
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))         # Create a result array, which will be the output 
+                                        # This will contain as much arrays as the length of input array
+        
+        prefix = 1                      # Initialize prefix as 1 
+        for i in range(len(nums)):      # and go through every position in the input array 
+            res[i] = prefix             # For each position in the result array 
+            prefix *= nums[i]           # Put the prefix in the result array position 
+                                        # So we can save memory from creating a prefix array 
+        
+        
+        postfix = 1                                 # Initialize postfix as 1 
+        for i in range(len(nums)  -1, -1, -1):      # now we go through backwards from the right side   
+            res[i] *= postfix                       # multiply postfix to the result array 
+            postfix *= nums[i]                      # continuously update the postfix by multiplying the                                                         # input array 
+        return res
