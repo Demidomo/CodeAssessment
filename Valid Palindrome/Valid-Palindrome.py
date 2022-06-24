b56fffd0c0e5572cc36e6e2d130a6be886e2eb19
Space_Garten <34130768+SpaceGarten@users.noreply.github.com>
2022-06-23 21:46:24 -0700

    Update Valid-Palindrome.py

--- Valid Palindrome/Valid-Palindrome.py
+++ Valid Palindrome/Valid-Palindrome.py
@@ -2,4 +2,15 @@
 # Return true if the string is a valid palindrome
 # The first and last character must be the same 
 # It must read the same way after it has been reversed
+# Ignore case and space for this problem
 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ""                 #Removing all the non alphanumeric characters from the original input string 
+        
+        for c in s: 
+            if c.isalnum():                    # If that character is alphanumerical, which we have a built in library to check
+                newStr += c.lower()            # Add it to the new string, making sure it is olower case 
+        return newStr == newStr[::-1]          # With the new string, we are just gonna compare it to the reverse string 
+        
+        
